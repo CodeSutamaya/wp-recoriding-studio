6c32da258d2d1c86227db0bcb8b4625ee7433d56
Benni HT <benni.kerja@gmail.com>
2022-10-04 13:59:59 +0700

    design single page

---
+++ wp-content/themes/lm-record-studio/single.php
@@ -0,0 +1,15 @@
+
+
+<?php get_header(); ?>
+
+<div id='cw' class="site-width">
+
+    <?php 
+    
+        if ($tipe=="sp-car") sp_car('images/1.jpg', '<span>2009</span> Audi A5 TDi Quattro S Line Satalite Navigation Convertible', '&#163;12,500', 'Diesel', 'Automatic', '82,000', '2007', 'Finished in metallic Phantom Black with full Black leather interior and a Black electric hood., Upgrades - Audi Music Interface (AMI), Metallic Paint, Bang & Olufsen Sound System, DVD Satellite Navigation, Heated Front Seats, Alloy Wheels-8.5Jx19in 5 Segment-Spoke Design, Mobile Telephone... <br/><br/>Section 1.10.32 of "de Finibus Bonorum et Malor"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."');
+        
+    ?>
+
+</div>
+
+<?php get_footer(); ?>
