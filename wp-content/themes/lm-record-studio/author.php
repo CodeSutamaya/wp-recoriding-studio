e316cd185746fc993dd6822ae6add523d15006b3
Benni HT <benni.kerja@gmail.com>
2022-10-05 16:51:55 +0700

    configure nav with backend

--- wp-content/themes/lm-record-studio/author.php
+++ wp-content/themes/lm-record-studio/author.php
@@ -1,5 +1,8 @@
-
-
 <?php get_header(); ?>
 <!-- design -->
+<a href="#" class="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600">
+    <span class="text-2xl"><i class="bx bx-home"></i></span>
+    <span>Dashboard</span>
+</a>
+
 <?php get_footer(); ?>
