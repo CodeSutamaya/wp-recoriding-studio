421964a9bc54e50a6a1e6b709fa31e3173be7e3c
Benni HT <benni.kerja@gmail.com>
2022-10-05 16:57:12 +0700

    input form done

--- wp-content/themes/lm-record-studio/author.php
+++ wp-content/themes/lm-record-studio/author.php
@@ -1,5 +1,14 @@
-
-
 <?php get_header(); ?>
 <!-- design -->
+<div class="solution-row">
+    <input type="text" placeholder="Telephone number" class="book-input" name="Telephone2" />
+</div>
+
+<div class="solution-row">
+    <input type="text" placeholder="E-mail address" class="book-input" name="Email2" />
+</div>
+
+<div class="solution-row-btn">
+    <input type="button" class="submit-pesan2 btn-book2" value="Book Now" name="Submit Book" />
+</div>
 <?php get_footer(); ?>
