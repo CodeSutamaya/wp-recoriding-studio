ed3f7a18d4d77695cae7cd688ee10381b5028a19
VKarada <valen.karada@gmail.com>
2022-10-06 13:06:01 +0700

    :sparkles: add sign-in

--- wp-content/themes/lm-record-studio/author.php
+++ wp-content/themes/lm-record-studio/author.php
@@ -5,5 +5,13 @@
     <span class="text-2xl"><i class="bx bx-home"></i></span>
     <span>Dashboard</span>
 </a>
+<div>
+    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+    <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 21-days free trial</a>
+    </p>
+</div>
 
 <?php get_footer(); ?>
